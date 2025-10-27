6aef5a9a54bb7e13a348a845d530cbc892362789
MircaMircas <mikosmikus@gmail.com>
2025-10-27 20:35:33 +0100

    Update make.sh to use s2swbuilt.bin

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-if test -f s2built.bin; then
-	mv -f s2built.bin s2built.prev.bin
+if test -f s2swbuilt.bin; then
+	mv -f s2swbuilt.bin s2swbuilt.prev.bin
 fi
 ./tool/linux/asl -xx -q -A -L -U -E -i . main.asm
-./tool/linux/p2bin -z=0,saxman-bugged,Size_of_DAC_driver_guess,after main.p s2built.bin
+./tool/linux/p2bin -z=0,saxman-bugged,Size_of_DAC_driver_guess,after main.p s2swbuilt.bin
 rm -f main.p
